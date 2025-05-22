ee4e1fddf39ee7f8d5ac09fee9f7030ae9a6d364
vomolo <valentineomolo1@gmail.com>
2025-05-22 21:51:10 +0300

    Add initial users table schema

---
+++ forum/database/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nickname TEXT UNIQUE NOT NULL,
+    age INTEGER NOT NULL,
+    gender TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
